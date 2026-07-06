8d9a63a28393bfb321695d31facc02fbbb01e28f
vFel1x <1830558588@qq.com>
2026-07-07 02:44:57 +0800

    Add files via upload

---
+++ my_first_C_code.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello world,hello Geman!\n");
+    printf("I'm vFel1x and today my journey from C Language to Electronic Information begins.\n");
+    printf("2026-7-6\n");
+    return 0;
+} 
