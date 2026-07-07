640e6337b20ba582c7bee6f77a0d8d0e564b7c1b
vFel1x <1830558588@qq.com>
2026-07-08 03:39:33 +0800

    Add files via upload

---
+++ variables_and_input.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int height;
+    int score;
+    printf("Please enter your height(cm):\n");
+    scanf("%d",&height);
+    printf("OK your height is %d\n",height);
+
+    printf("Now please enter your score(integers):\n");
+    scanf("%d",&score); 
+    printf("So your score is only %d\n",score);
+
+    printf("Pray that your grades are just as impressive as your height.\n");
+    return 0;
+}
